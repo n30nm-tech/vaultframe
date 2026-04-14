ff561facfec66d1a07eb93a597b3a3678a94016d
n30nm <n30nm@proton.me>
2026-04-14 11:39:39 +0100

    Fix instrumentation scan runner import

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,11 +1,8 @@
+import { ensureScanRunnerStarted } from "./lib/server/library-scan";
+
 export async function register() {
   if (process.env.NEXT_RUNTIME !== "nodejs") {
     return;
   }
-
-  const importScanRunner = new Function('return import("./lib/server/library-scan");') as () => Promise<{
-    ensureScanRunnerStarted: () => void;
-  }>;
-  const { ensureScanRunnerStarted } = await importScanRunner();
   ensureScanRunnerStarted();
 }
